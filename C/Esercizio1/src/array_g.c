09289a1df2dab377ddcec006fe6bd753ef8a3686
Roberto Esposito <roberto.esposito@unito.it>
2016-04-01 18:08:33 +0200

    Improvements in array_g functions implementation

--- C/Esercizio1/src/array_g.c
+++ C/Esercizio1/src/array_g.c
@@ -1,8 +1,13 @@
 #include "array_g.h"
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef unsigned char uchar;
+
+#define SWAP_BUFFER_SIZE 4096
+static uchar SWAP_BUFFER[SWAP_BUFFER_SIZE];
+
 
 void* at_g(const void* array, int pos, int size) {
   const uchar* carray = (const uchar*) array;
@@ -14,10 +19,9 @@ void cp_g(void* dst, const void* src, int size) {
 }
 
 void swap_g(void* el1, void* el2, int size) {
-  assert(size <= 1024);
-  uchar buf[1024];
+  assert(size <= SWAP_BUFFER_SIZE);
 
-  cp_g(buf, el1, size);
+  cp_g(SWAP_BUFFER, el1, size);
   cp_g(el1, el2, size);
-  cp_g(el2, buf, size);
+  cp_g(el2, SWAP_BUFFER, size);
 }
