c7a5bd37790d1eace98e976c11408e1fe8a33f48
Roberto Esposito <roberto.esposito@unito.it>
2016-05-16 14:02:27 +0200

    Implemented assert_pointer_not_equal

--- C/include/unit_testing.h
+++ C/include/unit_testing.h
@@ -28,6 +28,10 @@
    printf("\nassertion failed at file:%s line:%d. %ld was not expected to be equal to %ld\n",\
     __FILE__, __LINE__, (a), (b)); exit(1); }
 
+#define assert_pointers_not_equal(a,b) if((a)==(b)) {\
+   printf("\nassertion failed at file:%s line:%d. %p was not expected to be equal to %p\n",\
+    __FILE__, __LINE__, (a), (b)); exit(1); }
+
 #define assert_equal32(a,b) if((a)!=(b)) {\
    printf("\nassertion failed at file:%s line:%d. %d was expected to be equal to %d\n",\
     __FILE__, __LINE__, (a), (b)); exit(1); }
