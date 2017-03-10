a882dbc96858cfdcc8d64799c2ac631da6ea312e
Roberto Esposito <roberto.esposito@unito.it>
2017-03-10 10:37:31 +0100

    Separated iterator functions into their own header fil

---
+++ C/include/iterator_functions.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "iterator.h"
+#include "array.h"
+
+// Calls callback on each element of the container iterated by the given Iterator
+void for_each(Iterator, void (^callback)(void* elem));
+void for_each_with_index(Iterator it, void(^callback)(void*, size_t));
+
+// Finds the first element matching the condition evaluated by the callback
+// `condition` must evaluate to 1 if the given elem is the one sought.
+// it must evaluate to 0 otherwise.
+void* find_first(Iterator, int(^condition)(void* elem));
+
+// FIXME: TO BE IMPLEMENTED
+Array map(Iterator, size_t, void* (^)(void*));
+void* reduce(Iterator, void* zero, void* (^)(void* accumulator, void* element) );
+
+Array filter(Iterator, size_t, int (^)(void*));
