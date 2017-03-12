361c59f958fab9dade86590a46066b70928f7003
Roberto Esposito <roberto.esposito@unito.it>
2017-03-12 15:38:22 +0100

    Improved double container interface

--- C/include/double_container.h
+++ C/include/double_container.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // DoubleContainer* opaque type
-typedef struct _DoubleContainer DoubleContainer;
+typedef void DoubleContainer;
 
 // Creates a new container for the given double
 DoubleContainer* DoubleContainer_new(double value);
--- C/src/double_container.c
+++ C/src/double_container.c
@@ -2,19 +2,17 @@
 #include "double_container.h"
 #include "mem.h"
 
-struct _DoubleContainer {
-  double value;
-};
 
 DoubleContainer* DoubleContainer_new(double value) {
-  DoubleContainer* result = (DoubleContainer*) Mem_alloc(sizeof(struct _DoubleContainer));
-  result->value = value;
+  double* result = (double*) Mem_alloc(sizeof(double));
+  *result = value;
   return result;
 }
+
 void DoubleContainer_free(DoubleContainer* const dc) {
   Mem_free(dc);
 }
 
 double DoubleContainer_get(const DoubleContainer* dc) {
-  return dc->value;
+  return *(const double*) dc;
 }
