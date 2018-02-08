01b53c5f0b5aa7ec56ce61a1c885be5ace1136b8
Roberto Esposito <roberto.esposito@unito.it>
2018-02-08 18:18:07 +0100

    Add comments to editing_distance.c

--- C/src/editing_distance.c
+++ C/src/editing_distance.c
@@ -4,73 +4,14 @@
 #include <limits.h>
 #include "editing_distance.h"
 
-typedef struct {
-  unsigned long* matrix;
-  unsigned long len1;
-  unsigned long len2;
-} EDMemo;
-
-
-static unsigned long EDMemo_get(EDMemo* memo, unsigned long i, unsigned long j) {
-  return memo->matrix[i * (memo->len2 + 1) + j];
-}
-
-static void EDMemo_set(EDMemo* memo, unsigned long i, unsigned long j, unsigned long val) {
-  memo->matrix[i * (memo->len2 + 1) + j] = val;
-}
-
-
-__attribute__((unused)) static void EDMemo_print(EDMemo* memo) {
-  for(unsigned long i = 0; i < memo->len1 + 1; ++i) {
-    for(unsigned long j = 0; j < memo->len2 + 1; ++j) {
-      unsigned long value = EDMemo_get(memo, i, j);
-      printf("%2d\t", value == ULONG_MAX ? -1 : (int) value);
-    }
-
-    printf("\n");
-  }
-}
-
-
-static unsigned long EDMemo_memoize(EDMemo* memo, unsigned long i, unsigned long j, unsigned long (^compute_val)(void) ) {
-  unsigned long result = EDMemo_get(memo, i, j);
-  if(result == ULONG_MAX) {
-    result = compute_val();
-    EDMemo_set(memo, i, j, result);
-  }
-
-  return result;
-}
-
-static EDMemo* EDMemo_new(unsigned long len1, unsigned long len2) {
-  __block EDMemo* memo = (EDMemo*) malloc(sizeof(EDMemo));
-  memo->len1 = len1;
-  memo->len2 = len2;
-  memo->matrix = (unsigned long*) malloc( (len1 + 1) * (len2 + 1) * sizeof(unsigned long) );
-
-  for(unsigned long i=0; i < len1 + 1; ++i) {
-    for(unsigned long j=0; j < len2 + 1; ++j) {
-      EDMemo_set(memo,i,j,ULONG_MAX);
-    }
-  }
-
-  return memo;
-}
-
-static void EDMemo_free(EDMemo* memo) {
-  free(memo->matrix);
-  free(memo);
-}
-
-
-static unsigned long min(unsigned long x, unsigned long y) {
-  if(x<y) {
-    return x;
-  } else {
-    return y;
-  }
-}
-
+//
+// Implementation of the EditingDistance measure
+//
+// The starting point on which the implementation is based is the following
+// recursive function. Which works, but unfortunately it has an exponential cost.
+// The implementation adds memoization to this algorithm bringing the cost to linear.
+//
+//
 // static unsigned long editing_distance_dummy(const char* string1, const char* string2) {
 //   if(string1[0] == '\0') {
 //     return strlen(string2);
@@ -89,6 +30,84 @@ static unsigned long min(unsigned long x, unsigned long y) {
 //   }
 // }
 
+
+//  Definition of the structure used to memoize intermediate values
+typedef struct {
+  unsigned long* matrix;
+  unsigned long len1;
+  unsigned long len2;
+} EDMemo;
+
+// Return the value in position i,j
+static unsigned long EDMemo_get(EDMemo* memo, unsigned long i, unsigned long j) {
+  return memo->matrix[i * (memo->len2 + 1) + j];
+}
+
+// Set the value in position i,j to the content of `val`
+static void EDMemo_set(EDMemo* memo, unsigned long i, unsigned long j, unsigned long val) {
+  memo->matrix[i * (memo->len2 + 1) + j] = val;
+}
+
+// Debugging function: it prints the content of the memoization matrix
+__attribute__((unused)) static void EDMemo_print(EDMemo* memo) {
+  for(unsigned long i = 0; i < memo->len1 + 1; ++i) {
+    for(unsigned long j = 0; j < memo->len2 + 1; ++j) {
+      unsigned long value = EDMemo_get(memo, i, j);
+      printf("%2d\t", value == ULONG_MAX ? -1 : (int) value);
+    }
+
+    printf("\n");
+  }
+}
+
+// Memoization operator
+// If element in position i,j has already been set to a sensible value, it just
+// returns that value. Otherwise it calls `compute_val` to compute a new value
+// for those coordinates, memorize that value in position i,j and returns the computed value.
+
+static unsigned long EDMemo_memoize(EDMemo* memo, unsigned long i, unsigned long j, unsigned long (^compute_val)(void) ) {
+  unsigned long result = EDMemo_get(memo, i, j);
+  if(result == ULONG_MAX) {
+    result = compute_val();
+    EDMemo_set(memo, i, j, result);
+  }
+
+  return result;
+}
+
+// Creates a new memoization structure
+static EDMemo* EDMemo_new(unsigned long len1, unsigned long len2) {
+  __block EDMemo* memo = (EDMemo*) malloc(sizeof(EDMemo));
+  memo->len1 = len1;
+  memo->len2 = len2;
+  memo->matrix = (unsigned long*) malloc( (len1 + 1) * (len2 + 1) * sizeof(unsigned long) );
+
+  for(unsigned long i=0; i < len1 + 1; ++i) {
+    for(unsigned long j=0; j < len2 + 1; ++j) {
+      EDMemo_set(memo,i,j,ULONG_MAX);
+    }
+  }
+
+  return memo;
+}
+
+// Free the memoization structure
+static void EDMemo_free(EDMemo* memo) {
+  free(memo->matrix);
+  free(memo);
+}
+
+// Min function
+static unsigned long min(unsigned long x, unsigned long y) {
+  if(x<y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+// Uses memoization to compute the editing distance between string1 and string2 assuming
+// that someone already took care of all string1[0..i] and string2[0..j].
 static unsigned long editing_distance_with_memo(EDMemo* memo, const char* string1, const char* string2, unsigned long i, unsigned long j) {
   if(i==memo->len1) {
     return memo->len2 - j;
@@ -118,6 +137,7 @@ static unsigned long editing_distance_with_memo(EDMemo* memo, const char* string
 }
 
 
+// Returns the editing distance between string1 and string2
 unsigned long editing_distance(const char* string1, const char* string2) {
   EDMemo* memo = EDMemo_new(strlen(string1), strlen(string2));
   unsigned long result = editing_distance_with_memo(memo, string1, string2, 0, 0);
