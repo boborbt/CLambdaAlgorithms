1fd128fed8fd27b312de2eb8c0fd17a3727b6834
Roberto Esposito <roberto.esposito@unito.it>
2018-04-05 11:20:38 +0200

    Improved documentation.

--- C/include/basic_iterators.h
+++ C/include/basic_iterators.h
@@ -1,8 +1,33 @@
 #pragma once
 
 #include <stdio.h>
-
 #include "iterator.h"
+
+// BASIC ITERATORS
+// 
+// basic iterators are iterators over a number of basic types. They are useful
+// to allow using the iterator functiosn (for_each, sort, reverse, ...) onto objects
+// that are not dealt with the provided containers. For intance:
+//
+//  -  Number_it: can be used to iterate a block a fixed number of times, e.g.
+//    ```
+//      for_each(Number_it(100), ^(void* obj){
+//        printf("%ld\n", NUM(obj));
+//      });
+//    ```
+//  - TextFile_it: can be used to read the content of a file, e.g.:
+//    ```
+//      for_each(TextFile_it("myfile.txt", '\n'), ^(void* obj){
+//        printf("%s\n", (char*) buf);
+//      });
+//    ```
+//
+//   - Char_it: can be used to iterate over a string, e.g.:
+//    ```
+//      reverse(Char_it(mystring));
+//    ```
+
+
 
 // This is a convenience macro to dereference the objects returned by
 // iterations started using Number_it
