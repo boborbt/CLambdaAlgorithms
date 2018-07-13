bc237cb54d6ec8c5712ee814afee614203874e02
Roberto Esposito <roberto.esposito@unito.it>
2018-07-13 14:29:28 +0200

    Documentation fixes

--- C/include/kruskal.h
+++ C/include/kruskal.h
@@ -9,9 +9,9 @@ typedef struct _Kruskal Kruskal;
 // Creates and initializes the data structures used by the Dijkstra* algorithm
 Kruskal* Kruskal_new(Graph* graph, double (*graph_info_to_double)(const void*));
 
-// Runs Kruskal* and returns the found minimal spanning tree
-// The minimal path is returned as a newly alloced graph having the same
-// KeyInfo* as the source graph
+// Runs Kruskal algorithm and returns the found minimal spanning forest
+// The minimal spanning forest is returned as a newly alloced graph having the same
+// KeyInfo* as the source graph.
 // The user needs to dealloc the graph when finished with it.
 Graph* Kruskal_mintree(Kruskal*);
 
--- C/include/prim.h
+++ C/include/prim.h
@@ -9,8 +9,9 @@ typedef struct _Prim Prim;
 // Creates and initializes the data structures used by the Prim* algorithm
 Prim* Prim_new(Graph* graph, double (*graph_info_to_double)(const void*));
 
-// Runs Prim* and returns the found minimal spanning tree
-// The minimal path is returned as a newly alloced graph having the same
+// Runs Prim and returns the found minimal spanning forest starting from the given
+// node.
+// The minimal spanning forest is returned as a newly alloced graph having the same
 // KeyInfo* as the source graph
 // The user needs to dealloc the graph when he is finished with it.
 Graph* Prim_mintree(Prim*, void*);
