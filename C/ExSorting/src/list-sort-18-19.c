8cca57637ef56e4266145bd4ebdf08a1e380e81b
boborbt <boborbt@gmail.com>
2019-02-25 18:05:08 +0100

    Improved main program for list-sort-18-19

--- C/ExSorting/src/list-sort-18-19.c
+++ C/ExSorting/src/list-sort-18-19.c
@@ -3,8 +3,64 @@
 #include "basic_iterators.h"
 #include "iterator_functions.h"
 #include "mem.h"
+#include "macros.h"
 #include <assert.h>
 #include <stdlib.h>
+
+typedef enum {
+    SORT,
+    MERGE
+} Commands;
+
+typedef struct {
+    Commands command;
+    const char* program_name;
+    const char* file_name1;
+    const char* file_name2;
+} Options;
+
+static void print_usage(const char **argv) {
+  printf("Usage: \n");
+  printf("  %s -s <filename>\n", argv[0]);
+  printf("or\n");
+  printf(" %s -m <list1> <list2>\n", argv[0]);
+}
+
+static Options parse_options(int argc, char const **argv) {
+  if (argc < 3) {
+      print_usage(argv);
+      exit(1);
+  }
+
+  Options result;
+  result.program_name = argv[0];
+
+  if( !strcmp(argv[1], "-s" ) ) {
+      result.command = SORT;
+      result.file_name1 = argv[2];
+      result.file_name2 = NULL;
+      return result;
+  }
+
+  if( !strcmp(argv[1], "-m") ) {
+      if(argc < 4) {
+          printf("Too few parameters for -m option.\n");
+          print_usage(argv);
+          exit(2);
+      }
+
+      result.command = MERGE;
+      result.file_name1 = argv[2];
+      result.file_name2 = argv[3];
+      return result;
+  }
+
+  printf("Unrecognized parameter. Expected -m or -s and got '%s'", argv[1]);
+  exit(3);
+}
+
+
+
 
 static void list_split(List* src, List* lhs, List* rhs) {
     __block int index = 0;
@@ -104,38 +160,97 @@ static void mem_free(void* ptr) {
     Mem_free(ptr);
 }
 
-int main(int argc, char const *argv[]) {
-    if(argc < 2) {
-        printf("Usage: \n");
-        printf("  %s <filename>", argv[0]);
-        exit(1);
-    }
+static void do_sort(Options options) {
+  PrintTime *pt = PrintTime_new(NULL);
+  PrintTime_add_header(pt, "esercizio", "sort");
+  PrintTime_add_header(pt, "invocation", options.program_name);
 
-    PrintTime* pt = PrintTime_new(NULL);
-    PrintTime_add_header(pt, "esercizio", "1");
-    PrintTime_add_header(pt, "invocation", argv[0]);
+  List *list = load_dataset(pt, options.file_name1);
 
-    List *list = load_dataset(pt, argv[1]);
+  PrintTime_print(pt, "Sorting...", ^{
+    list_sort(list, ^(void *lhs, void *rhs) {
+      long int lhs_val = *(long int *)lhs;
+      long int rhs_val = *(long int *)rhs;
 
-
-    PrintTime_print(pt, "Sorting...", ^{
-        list_sort(list, ^(void* lhs, void* rhs) {
-            long int lhs_val = *(long int*) lhs;
-            long int rhs_val = *(long int*) rhs;
-
-            if(lhs_val < rhs_val) {
-                return -1;
-            } else if(lhs_val == rhs_val) {
-                return 0;
-            } else {
-                return 1;
-            }
-        });
+      if (lhs_val < rhs_val) {
+        return -1;
+      } else if (lhs_val == rhs_val) {
+        return 0;
+      } else {
+        return 1;
+      }
     });
 
-    PrintTime_free(pt);
+    __block ListNode *it = List_head(list);
+    for_each(Number_it(10), ^(UNUSED(void *index)) {
+      long int elem = *(long int *)ListNode_get(list, it);
+      printf("%ld\n", elem);
+      it = List_next(list, it);
+    });
+  });
 
-    List_free(list, mem_free);
+  PrintTime_free(pt);
+
+  List_free(list, mem_free);
+}
+
+static void do_merge(Options options) {
+  PrintTime *pt = PrintTime_new(NULL);
+  PrintTime_add_header(pt, "esercizio", "merge");
+  PrintTime_add_header(pt, "invocation", options.program_name);
+
+  List *list1 = load_dataset(pt, options.file_name1);
+  List *list2 = load_dataset(pt, options.file_name2);
+
+  PrintTime_print(pt, "Merging...", ^{
+    printf("Merging...\n");
+    __block List *dest = List_new();
+    size_t len1 = List_size(list1);
+    size_t len2 = List_size(list2);
+    for_each(Number_it(len1+len2), ^(UNUSED(void* number)) {
+        List_append(dest, NULL);
+    });
+
+    list_merge(dest, list1, list2, ^(void *lhs, void *rhs) {
+      long int lhs_val = *(long int *)lhs;
+      long int rhs_val = *(long int *)rhs;
+
+      if (lhs_val < rhs_val) {
+        return -1;
+      } else if (lhs_val == rhs_val) {
+        return 0;
+      } else {
+        return 1;
+      }
+    });
+
+    __block ListNode* it = List_head(dest);
+    for_each(Number_it(10), ^(UNUSED(void* index)) {
+        long int elem = *(long int*) ListNode_get(dest, it);
+        printf("%ld\n", elem);
+        it = List_next(dest, it);
+    });
+
+    List_free(dest, NULL);
+  });
+
+  PrintTime_free(pt);
+
+  List_free(list1, mem_free);
+  List_free(list2, mem_free);
+}
+
+int main(int argc, char const *argv[]) {
+    Options options = parse_options(argc, argv);
+
+    switch(options.command) {
+        case SORT:
+            do_sort(options);
+            break;
+        case MERGE:
+            do_merge(options);
+            break;
+    }
 
     Mem_check_and_report();
     return 0;
