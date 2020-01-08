b98fb2860424f10b8e9ce8e14463ae0c715c880b
Roberto Esposito <roberto.esposito@unito.it>
2020-01-08 11:40:57 +0100

    Removed warnings found by -Weverything

--- optimal_connectivity.c
+++ optimal_connectivity.c
@@ -21,8 +21,8 @@ typedef struct _Edge {
 } Edge;
 
 typedef struct _Edges {
-  Edge** edges;
   int num_edges;
+  Edge** edges;
 } Edges;
 
 typedef struct _Node {
@@ -52,11 +52,12 @@ typedef struct _Tree {
   /// ancestor to its 2^(i-1)-th ancestor
   int max_weights[NUM_NODES][LOG_NUM_NODES];
 
+  /// size of the tree
+  int size;
+
   /// list of adjacency, adj[u] contains the list of adjacents of node u
   Node** adj;
 
-  /// size of the tree
-  int size;
 } Tree;
 
 // type of list iterators.
@@ -66,15 +67,16 @@ typedef Node ListIt;
 
 /// MARK: -------------------------   LIST   -------------------------
 
-static int list_len(Node* list) {
-  int count = 0;
-  while (list != NULL) {
-    list = list->next;
-    count++;
-  }
+// ..Unused..
+// static int list_len(Node* list) {
+//   int count = 0;
+//   while (list != NULL) {
+//     list = list->next;
+//     count++;
+//   }
 
-  return count;
-}
+//   return count;
+// }
 
 static Node* list_add(Node* list, int elem, int weight) {
   Node* node = (Node*)malloc(sizeof(Node));
@@ -128,7 +130,7 @@ static int list_iterator_get_weight(ListIt* it) { return it->weight; }
 // Initializes the tree. It assumes that the main structure has already been
 // alloced.
 static void tree_init(Tree* tree, int num_nodes) {
-  tree->adj = (Node**)calloc(num_nodes + 1, sizeof(Node*));
+  tree->adj = (Node**)calloc((unsigned long)num_nodes + 1, sizeof(Node*));
   tree->size = num_nodes;
 
   for (int u = 0; u < NUM_NODES; ++u) {
@@ -193,11 +195,11 @@ static void read_edges(Tree* tree) {
 }
 
 // Reads the query list from the stdin
-static void read_queries(Edges* queries, Tree* tree) {
+static void read_queries(Edges* queries) {
   queries->num_edges = read_num_edges();
   require(queries->num_edges < NUM_NODES);
 
-  queries->edges = (Edge**)malloc(sizeof(Edge*) * queries->num_edges);
+  queries->edges = (Edge**)malloc(sizeof(Edge*) * (unsigned long) queries->num_edges);
   require(queries->edges != NULL);
 
   for (int i = 0; i < queries->num_edges; ++i) {
@@ -211,7 +213,7 @@ static void read_queries(Edges* queries, Tree* tree) {
 /// MARK: -------------------------   ALGORITHM   -------------------------
 
 // Returns the 2^ith ancestor of u found during the dfs visit
-int ancestor(Tree* tree, int u, int i) {
+static int ancestor(Tree* tree, int u, int i) {
   require(u <= tree->size);
   require(i < LOG_NUM_NODES);
 
@@ -219,7 +221,7 @@ int ancestor(Tree* tree, int u, int i) {
 }
 
 // Returns the max weight of edges on the path from u to its 2^ith ancestor
-int maxval(Tree* tree, int u, int i) {
+static int maxval(Tree* tree, int u, int i) {
   require(u <= tree->size);
   require(i < LOG_NUM_NODES);
   return tree->max_weights[u][i];
@@ -231,7 +233,7 @@ int maxval(Tree* tree, int u, int i) {
 ///   - the list of 2^i-th ancestors for each node and for 0<=i<LOG_NUM_NODES
 ///   - the list of maximal weights for the ancestors
 
-void depth_first_search(Tree* tree, int u) {
+static void depth_first_search(Tree* tree, int u) {
   require(u <= tree->size);
   // starts by updating ancestors and max weight for the current node, we assume
   // that the 0-th ancestor and the 0-th max weight have already been set and
@@ -248,28 +250,36 @@ void depth_first_search(Tree* tree, int u) {
 
   ListIt* it = list_iterator(tree->adj[u]);
   while (it != NULL) {
-    if (tree->depth[it->elem] != -1) {
+    if (tree->depth[list_iterator_get(it)] != -1) {
       it = list_iterator_next(it);
       continue;
     }
 
     // sets the depth, the 0-th ancestor, and the 0-th max-value for the
     // next node;
-    tree->depth[it->elem] = tree->depth[u] + 1;
-    tree->ancestors[it->elem][0] = u;
-    tree->max_weights[it->elem][0] = it->weight;
-    depth_first_search(tree, it->elem);
+    int elem = list_iterator_get(it);
+    tree->depth[elem] = tree->depth[u] + 1;
+    tree->ancestors[elem][0] = u;
+    tree->max_weights[elem][0] = list_iterator_get_weight(it);
+    depth_first_search(tree, elem);
 
     it = list_iterator_next(it);
   }
 }
 
-int lca_raise_u_to_equalize_levels(Tree* tree, int u, int v, int* max_value) {
+static int ilog2(int val) {
+  int ilog = 0;
+  while(val >>= 1) ++ilog;
+
+  return ilog;
+}
+
+static int lca_raise_u_to_equalize_levels(Tree* tree, int u, int v, int* max_value) {
   // first we need to get both nodes at the same level. We do that
   // by raising the largest one until we get to the same level
   int depth_diff = tree->depth[u] - tree->depth[v];
   while (depth_diff > 0) {
-    int log_diff = log2(depth_diff);
+    int log_diff = ilog2(depth_diff);
 
     *max_value = max(*max_value, maxval(tree, u, log_diff));
     u = ancestor(tree, u, log_diff);
@@ -281,7 +291,7 @@ int lca_raise_u_to_equalize_levels(Tree* tree, int u, int v, int* max_value) {
 
 // Sets u and v so to point to the query edge vertices and so that
 // depth[u] > depth[v]
-void lca_set_u_v(Tree* tree, int* u, int* v, Edge* query) {
+static void lca_set_u_v(Tree* tree, int* u, int* v, Edge* query) {
   if (tree->depth[query->source] > tree->depth[query->dest]) {
     *u = query->source;
     *v = query->dest;
@@ -298,7 +308,7 @@ void lca_set_u_v(Tree* tree, int* u, int* v, Edge* query) {
 // the nodes u and v. If m_u is the max weight on the path from
 // u to lca(u,v) and m_v is the max weight on the path from v to lca(u,v),
 // then max(m_u,m_v) is the max weight on the path from u to v.
-int lca_max_weight(Tree* tree, Edge* query) {
+static int lca_max_weight(Tree* tree, Edge* query) {
   require(query->source <= tree->size);
   require(query->dest <= tree->size);
 
@@ -313,7 +323,7 @@ int lca_max_weight(Tree* tree, Edge* query) {
   }
 
   // now we can raise both until we get to the lca
-  int level = log2(tree->depth[u]);
+  int level = ilog2(tree->depth[u]);
   while (level >= 0) {
     if (ancestor(tree, u, level) != -1 &&
         ancestor(tree, u, level) != ancestor(tree, v, level)) {
@@ -331,14 +341,14 @@ int lca_max_weight(Tree* tree, Edge* query) {
 
 /// MARK: -------------------------   MAIN   -------------------------
 
-void main_work() {
+static void main_work() {
   Tree* tree = (Tree*)malloc(sizeof(Tree));
   Edges* queries = (Edges*)malloc(sizeof(Edges));
   require(tree != NULL);
   require(queries != NULL);
 
   read_edges(tree);
-  read_queries(queries, tree);
+  read_queries(queries);
 
   int orig = queries->edges[0]->source;
   tree->depth[orig] = 0;
@@ -358,7 +368,7 @@ void main_work() {
   free(queries);
 }
 
-void open_files(int argc, char** argv) {
+static void open_files(int argc, char** argv) {
   if (argc > 1) {
     FILE* filein = fopen(argv[1], "r");
     stdin = filein;
@@ -370,7 +380,7 @@ void open_files(int argc, char** argv) {
   }
 }
 
-void close_files(int argc, char** argv) {
+static void close_files(int argc) {
   if (argc > 1) {
     fclose(stdin);
   }
@@ -387,6 +397,6 @@ int main(int argc, char** argv) {
 
   main_work();
 
-  close_files(argc, argv);
+  close_files(argc);
   return 0;
 }
