db67231712cceadeab85531c65daf167607a3cb0
Roberto Esposito <roberto.esposito@unito.it>
2022-11-22 22:10:56 +0100

    Code documentation (doxygen)

--- C/include/array.h
+++ C/include/array.h
@@ -4,122 +4,191 @@
 #include "iterator.h"
 #include "keys.h"
 
+/**
+ * @file Array
+ * @brief The Array structure implements a dynamic array and supports all iterator traits.
+ *
+ * The array can only contain pointers to data and it will not free the data when the array is freed.
+ * 
+ * **Example**
+ *
+ * ```c
+ * #include "array.h"
+ * #include "iterator_functions.h"
+ * #include <stdio.h>
+ * #include <stdlib.h>
+ * 
+ * int main() {
+ *     // Create a new array with initial capacity of 10
+ *     Array* array = Array_new(10);
+ * 
+ *     for (int i = 0; i < 10; i++) {
+ *         int* val = malloc(sizeof(int));
+ *         *val = i;
+ *         Array_add(array, val);
+ *     }
+ * 
+ *     for_each(Array_it(array), ^(void* element) {
+ *         printf("%d ", *(int*)element);
+ *     });
+ * 
+ *     for_each(Array_it(array), ^(void* element) {
+ *         free(element);
+ *     });
+ * 
+ *     Array_free(array);
+ *     return 0;
+ * }
+ * ```
+ */
+
 typedef struct _Array Array;
 typedef struct _ArrayIterator ArrayIterator;
 
 // Constructors
+
+/// @brief  Creates a new array.
+/// @param capacity The initial capacity of the array.
+/// @return A pointer to the newly created array.
 Array* Array_new(size_t capacity);
+
+/// @brief Creates a new array from an existing C array.
+/// @param array the C array to copy.
+/// @param size the size of the C array.
+/// @return  A pointer to the newly created array.
 Array* Array_new_by_copying_carray(void* array, size_t size);
+
+/// @brief Creates a new array from an existing C array.
+/// @param array the C array to copy.
+/// @return  A pointer to the newly created array.
 Array* Array_dup(Array*);
 
 // Destructor
+
+/// @brief Frees the memory allocated by the array.
+/// @param array The array to free.
 void Array_free(Array*);
 
 //
 // Accessors
 //
 
-// Returns 1 if a1 and a2 contains the same objects, 0 otherwise.
+/// @brief Returns 1 if a1 and a2 contains the same objects, 0 otherwise.
+/// @param a1 The first array.
+/// @param a2 The second array.
+/// @return 1 if a1 and a2 contains the same objects, 0 otherwise. Equality is determined by pointer equality.
 int Array_same(Array* a1, Array* a2);
 
 
-// Returns a pointer to the object at the given index. It raises an ERROR_INDEX_OUT_OF_BOUND
-// if index is not in bounds. Bounds are [0, Array_size(array)-1]
+/// @brief returns a pointer to the object at the given index. 
+/// @param array The array.
+/// @param index The index of the object to return.
+/// @return A pointer to the object at the given index.
+/// @warning The index must be within the bounds of the array. It raises an ERROR_INDEX_OUT_OF_BOUND if index is not in bounds. 
+///          Bounds are [0, Array_size(array)-1]
 void* Array_at(Array* array, size_t index);
 
-// Returns a C array representation of the given array. Valid indices are
-// between 0 and Array_size(array)-1.
+/// @brief Returns a C array representation of the given array.
 void* Array_carray(Array* array);
 
-// Returns the number of elements in the current array
+/// @brief Returns the number of elements in the current array
 size_t Array_size(Array* array);
 
-// Returns true iff Array_size(array)==0
+/// @brief Returns true iff Array_size(array)==0
 int Array_empty(Array* array);
 
-// Returns the size in bytes of an element stored in the array
+/// @brief Returns the size in bytes of an element stored in the array
 size_t Array_elem_size(Array* array);
 
-// Returns the number of elements the array can accomodate without reallocating
-// to a larger structure.
+/// @brief Returns the number of elements the array can accomodate without reallocating
+/// to a larger structure.
 size_t Array_capacity(Array* array);
 
-// Set the size the array to the new size reallocing the elements if necessary
+/// @brier Set the size the array to the new size reallocing the elements if necessary
 void Array_set_size(Array* array, size_t new_size);
 
-// semantically equivalent to array[index] = elem (if array was a carray)
-// returns elem
+/// @brief semantically equivalent to array[index] = elem (if array was a carray)
+/// @brief returns elem
 void* Array_set(Array* array, size_t index, void* elem);
 
 
-// inserts elem at the given index and shifts all elements up one position.
-// it reallocates the array space if necessary
+/// @brief inserts elem at the given index and shifts all elements up one position.
+/// it reallocates the memory taken by the array if necessary
 void Array_insert(Array*, size_t index, void* elem);
 
-// appends elem to the given array.
+/// @brief appends elem to the given array.
+/// it reallocates the memory taken by the array if necessary
 void Array_add(Array* array, void* elem);
 
-// removes an element at a specific index from the array and shifts all
-// following elements down one position
+/// @brief removes an element at a specific index from the array and shifts all
+/// following elements down one position
 void Array_remove(Array* array, size_t index);
 
 
-// sorts in-place the given array. The parameters to the compare function
-// are pointers to the objects inserted into the array. This implies that
-// it is usually necessary a cast and a dereference to get the actual value:
-// e.g., if the array contains Record objects then the compare block
-//   should be something like:
-//
-// int (^compare)(const void* o1, const void* o2) {
-//   Record rec1 =  o1;
-//   Record rec2 =  o2;
-//
-//   ... do the comparison ...
-// }
-// 
-// the compare function should follow the strcmp pattern (0 means the two
-// objects are equal, -1 means that the first smaller than the second, and
-// +1 means that the first is larger than the second).
+/// @brief sorts in-place the given array. 
+/// The parameters to the compare function
+/// are pointers to the objects inserted into the array. This implies that
+/// it is usually necessary a cast and a dereference to get the actual value:
+/// e.g., if the array contains Record objects then the compare block
+///   should be something like:
+/// ```c
+/// int (^compare)(const void* o1, const void* o2) {
+///   Record rec1 =  o1;
+///   Record rec2 =  o2;
+///
+///   ... do the comparison ...
+/// }
+/// ```
+/// the compare function should follow the strcmp pattern (0 means the two
+/// objects are equal, -1 means that the first smaller than the second, and
+/// +1 means that the first is larger than the second).
 void Array_sort(Array* array, KIBlkComparator compare);
 
-// Iterator
+/// @brief Returns a new iterator over the array.
 ArrayIterator* ArrayIterator_new(Array*);
+
+/// @brief Frees the memory allocated by the iterator.
 void ArrayIterator_free(ArrayIterator*);
 
+/// @brief Returns the size of the array subject to iteration.
 size_t ArrayIterator_size(ArrayIterator*);
 
-// Move the iterator to the next element. Do nothing if it is already past the
-// end of the container.
+/// @brief Move the iterator to the next element. Do nothing if it is already past the
+/// end of the container.
 void ArrayIterator_next(ArrayIterator*);
 
 
-// Move the iterator to the previous element. Do nothing if it is already past the
-// end (i.e., beginning) of the container.
+/// @brief Move the iterator to the previous element. Do nothing if it is already past the
+/// end (i.e., beginning) of the container.
 void ArrayIterator_prev(ArrayIterator*);
 
+/// @brief Move the iterator to the specified index.
 void ArrayIterator_move_to(ArrayIterator*, size_t);
 
-// Moves the iterator to the begin of the container (i.e., on the first element)
+/// @brief Moves the iterator to the begin of the container (i.e., on the first element)
 void ArrayIterator_to_begin(ArrayIterator*);
 
-// Moves the iterator to the end of the container (i.e., on the last element)
+/// @brief Moves the iterator to the end of the container (i.e., on the last element)
 void ArrayIterator_to_end(ArrayIterator *);
 
-// Returns 1 if the iterator is past the end of the container (i.e., if
-// ArrayIterator_get would not return a sensible result), 0 otherwise.
+/// @brief Returns 1 if the iterator is past the end of the container (i.e., if
+/// ArrayIterator_get would not return a sensible result), 0 otherwise.
 int ArrayIterator_end(ArrayIterator* it);
 
-// Returns the element currently pointed by the iterator
+/// @brief Returns the element currently pointed by the iterator
 void* ArrayIterator_get(ArrayIterator* it);
 
-// Sets the current pointed element to the given value
+/// @brief Sets the current pointed element to the given value
 void ArrayIterator_set(ArrayIterator* it, void* value);
 
-// Returns 1 if it1 and it2 points to the same element in the array, 0 otherwise.
+/// @brief Returns 1 if it1 and it2 points to the same element in the array, 0 otherwise.
 int ArrayIterator_same(ArrayIterator* it1, ArrayIterator* it2);
 
-//
+
 // Iterator maker
 // It returns a mutable bidirectional random iterator.
 
+/// @brief Creates a new Iterator interface to the iterators supported by the Array.
+/// @param array 
+/// @return an Iterator over the array.
 Iterator Array_it(Array* array);
--- C/include/iterator.h
+++ C/include/iterator.h
@@ -2,80 +2,82 @@
 
 #include <stdlib.h>
 
-// The Iterator type is the basis for the cross-container support for iteration functions.
-// Every container should implement a functions that builds an Iterator for the container.
-// That functions should simply call Iterator_make in order to fill the details necessary
-// to support iteration over that container.
-//
-// Once such a function exist the container gain access to all iteration functions declared
-// in iterator_functions.h (e.g., for_each, map, find_first, ...).
+/// @file iterator.h
+/// @brief The Iterator type is the basis for the cross-container support for iteration functions.
+///
+/// Every container should implement a functions that builds an Iterator for the container.
+/// That functions should simply call Iterator_make in order to fill the details necessary
+/// to support iteration over that container.
+///
+/// Once such a function exist the container gain access to all iteration functions declared
+/// in iterator_functions.h (e.g., for_each, map, find_first, ...).
+///
+///
+/// Iterator type declaration
+///
+/// The Iterator type stores all the function pointers needed to work with the iterator APIs.
+/// The Iterator struct can be actually thought as a meta-iterator interface since will not
+/// hold the actual iterators, but it will provide means to created and work with them using
+/// an interface that will be shared by all iterator implementations.
+///
+/// The information that need to create a new Iterator is:
+/// - container: the actual container on which one needs to iterate
+/// - new_iterator: a function that creates a new iterator instance.
+///        input: the container on which to the iterator will operate;
+///        output: a new iterator instance
+/// - next: a function that advances the iterator object to the next element.
+///        input: an iterator instance
+/// - get: a function that dereferences the iterator and returns the object pointed by
+///        the iterator.
+///        input: the iterator instance
+/// - end: a function that returns true if the iterator has become invalid;
+///        input: the iterator instance
+///        output: 1 if the iterator is no longer valid (it points either before the beginning or
+///        past the end of the container)
+/// - same: a function that returns 1 if the given two iterators point to the same
+///        point in the same container and 0 otherwise
+/// - free: a function that takes in input the iterator object and deallocates it.
+///
+/// A random access iterator must also privide the following function pointers:
+/// - move_to: a function that moves the iterator to a new place.
+///       input: an instance of the iterator and the new position;
+/// - size: a function returning the size of the container.
+///       input: an instance of the iterator
+///       output: the size of the underlying container
+/// NOTE: move_to must be return a valid object every time it is called with a position
+///       belonging to the range [0, size-1]
+///
+/// a bidirectional iterator  must also privide the following function pointers:
+///
+/// - prev: a function that moves the iterator back of one position.
+///        input: the iterator instance;
+/// - to_end: a function that moves the iterator at the end of the container;
+///        input: the iterator instance
 
-
-// Iterator type declaration
-//
-// The Iterator type stores all the function pointers needed to work with the iterator APIs.
-// The Iterator struct can be actually thought as a meta-iterator interface since will not
-// hold the actual iterators, but it will provide means to created and work with them using
-// an interface that will be shared by all iterator implementations.
-//
-// The information that need to create a new Iterator is:
-// - container: the actual container on which one needs to iterate
-// - new_iterator: a function that creates a new iterator instance.
-//        input: the container on which to the iterator will operate;
-//        output: a new iterator instance
-// - next: a function that advances the iterator object to the next element.
-//        input: an iterator instance
-// - get: a function that dereferences the iterator and returns the object pointed by
-//        the iterator.
-//        input: the iterator instance
-// - end: a function that returns true if the iterator has become invalid;
-//        input: the iterator instance
-//        output: 1 if the iterator is no longer valid (it points either before the beginning or
-//        past the end of the container)
-// - same: a function that returns 1 if the given two iterators point to the same
-//        point in the same container and 0 otherwise
-// - free: a function that takes in input the iterator object and deallocates it.
-//
-// A random access iterator must also privide the following function pointers:
-// - move_to: a function that moves the iterator to a new place.
-//       input: an instance of the iterator and the new position;
-// - size: a function returning the size of the container.
-//       input: an instance of the iterator
-//       output: the size of the underlying container
-// NOTE: move_to must be return a valid object every time it is called with a position
-//       belonging to the range [0, size-1]
-//
-// a bidirectional iterator  must also privide the following function pointers:
-//
-// - prev: a function that moves the iterator back of one position.
-//        input: the iterator instance;
-// - to_end: a function that moves the iterator at the end of the container;
-//        input: the iterator instance
-
-// mutable iterators  must also privide the following function pointers:
-//
-// - set: a function that assign to the current pointed element the given value
-//      input: an instance of the iterator and the value to be assigned
-//
-// cloning iterators  must also privide the following function pointers:
-//
-// NOTE: cloning is only useful for value types. When a container stores pointers, cloning can
-// (and for performance reasons should) degrade to simply returning the pointers stored in the
-// container. To understand why this is the case, consider that the client of the api will store
-// the result of the cloning operation into a pointer. Since that storage is sufficient to actually
-// hold the data (i.e., the pointer in the container) it is not necessary to alloc memory just to
-// hold the pointer.
-//  - alloc_obj: a function that allocates enough memory to store a new instance of the objects
-//       contained in the underlying container
-//       input: an instance of the iterator
-//       output: NULL if no memory needs to be alloced or the address to the alloced memory otherwise
-//  - copy_obj: a function that stores the current object into the given memory and returns the given
-//          given memory;
-//       input: an interator instance and a pointer to the memory where to copy the data;
-//       output: the pointer to the container object (if the container store pointers) or the pointer
-//          received as input (in that case the function will have copied the current object into the
-//          pointed memory)
-//  - free_obj: frees the alloced memory (if necessary)
+/// mutable iterators  must also privide the following function pointers:
+///
+/// - set: a function that assign to the current pointed element the given value
+///      input: an instance of the iterator and the value to be assigned
+///
+/// cloning iterators  must also privide the following function pointers:
+///
+/// NOTE: cloning is only useful for value types. When a container stores pointers, cloning can
+/// (and for performance reasons should) degrade to simply returning the pointers stored in the
+/// container. To understand why this is the case, consider that the client of the api will store
+/// the result of the cloning operation into a pointer. Since that storage is sufficient to actually
+/// hold the data (i.e., the pointer in the container) it is not necessary to alloc memory just to
+/// hold the pointer.
+///  - alloc_obj: a function that allocates enough memory to store a new instance of the objects
+///       contained in the underlying container
+///       input: an instance of the iterator
+///       output: NULL if no memory needs to be alloced or the address to the alloced memory otherwise
+///  - copy_obj: a function that stores the current object into the given memory and returns the given
+///          given memory;
+///       input: an interator instance and a pointer to the memory where to copy the data;
+///       output: the pointer to the container object (if the container store pointers) or the pointer
+///          received as input (in that case the function will have copied the current object into the
+///          pointed memory)
+///  - free_obj: frees the alloced memory (if necessary)
 
 typedef struct _Iterator Iterator;
 
@@ -125,15 +127,16 @@ void require_cloning_iterator(Iterator it);
 
 
 
-// Helper for making Iterators
-// Input: the functions that collectively allows building an iterator,
-// iterating on a container using it, and destroying it.
-//
-// Note: the name of the function ending with "make" is to denote that this is
-//        not a "new" function and hence the Iterator needs not to be free.
-//
-// Note: the iterator object referenced below is the particular iterator object needed to
-//       iterate over that specific containers, it has nothing to do with the Iterator type.
+/// @brief Helper for making Iterators
+///
+/// Input: the functions that collectively allows building an iterator,
+/// iterating on a container using it, and destroying it.
+///
+/// Note: the name of the function ending with "make" is to denote that this is
+///        not a "new" function and hence the Iterator needs not to be free.
+///
+/// Note: the iterator object referenced below is the particular iterator object needed to
+///       iterate over that specific containers, it has nothing to do with the Iterator type.
 
 
 Iterator Iterator_make(
@@ -147,17 +150,17 @@ Iterator Iterator_make(
   void  (*_free)(void*)
 );
 
-// RandomAccessIterator APIs
-// A RandomAccessIterator supports additional functions over containers (e.g., binsearch)
-// To build a RandomAccessIterator you are supposed to invoke Iterator_make to intialize the
-// "standard" part of the iterator and then use it with `RandomAccessIterator_make` to fill-in
-// the RandomAccessIterator APIs.
-//
-// Example:
-// ```C
-//  Iterator it = Iterator_make(...);
-//  it = RandomAccessIterator_make(it, _my_move_to_fun, _my_size_fun);
-// ```
+/// @brief RandomAccessIterator APIs
+/// A RandomAccessIterator supports additional functions over containers (e.g., binsearch)
+/// To build a RandomAccessIterator you are supposed to invoke Iterator_make to intialize the
+/// "standard" part of the iterator and then use it with `RandomAccessIterator_make` to fill-in
+/// the RandomAccessIterator APIs.
+///
+/// Example:
+/// ```C
+///  Iterator it = Iterator_make(...);
+///  it = RandomAccessIterator_make(it, _my_move_to_fun, _my_size_fun);
+/// ```
 
 Iterator RandomAccessIterator_make(
   Iterator iterator,
@@ -165,19 +168,19 @@ Iterator RandomAccessIterator_make(
   size_t (*size)(void*)
 );
 
-// BidirectionalIterator APIs
-// A BidirectionalIterator supports additional functions over containers (e.g., iterating in reverse)
-// order.
-//
-// To build a BidirectionalIterator you are supposed to invoke Iterator_make to intialize the
-// "standard" part of the iterator and then use it with `BidirectionalIterator_make` to fill-in
-// the BidirectionalIterator APIs.
-//
-// Example:
-// ```C
-//  Iterator it = Iterator_make(...);
-//  it = BidirectionalIterator_make(it, _my_prev_fun _my_to_end_fun);
-// ```
+/// @brief BidirectionalIterator APIs
+/// A BidirectionalIterator supports additional functions over containers (e.g., iterating in reverse)
+/// order.
+///
+/// To build a BidirectionalIterator you are supposed to invoke Iterator_make to intialize the
+/// "standard" part of the iterator and then use it with `BidirectionalIterator_make` to fill-in
+/// the BidirectionalIterator APIs.
+///
+/// Example:
+/// ```C
+///  Iterator it = Iterator_make(...);
+///  it = BidirectionalIterator_make(it, _my_prev_fun _my_to_end_fun);
+/// ```
 
 
 Iterator BidirectionalIterator_make(
@@ -187,18 +190,18 @@ Iterator BidirectionalIterator_make(
 );
 
 
-// MutableIterator APIs
-// A mutable iterator supports the assignment of a new value to the iterated objects.
-//
-// To build a MutableIterator you are supposed to invoke Iterator_make to intialize the
-// "standard" part of the iterator and then use it with `MutableIterator_make` to fill-in
-// the MutableIterator APIs.
-//
-// Example:
-// ```C
-//  Iterator it = Iterator_make(...);
-//  it = MutableIterator_make(it, _my_set_fun);
-// ``
+/// @brief MutableIterator APIs
+/// A mutable iterator supports the assignment of a new value to the iterated objects.
+///
+/// To build a MutableIterator you are supposed to invoke Iterator_make to intialize the
+/// "standard" part of the iterator and then use it with `MutableIterator_make` to fill-in
+/// the MutableIterator APIs.
+///
+/// Example:
+/// ```C
+///  Iterator it = Iterator_make(...);
+///  it = MutableIterator_make(it, _my_set_fun);
+/// ``
 
 Iterator MutableIterator_make(
   Iterator iterator,
@@ -206,15 +209,15 @@ Iterator MutableIterator_make(
 );
 
 
-// CloningIterator
-// A cloning iterator supports cloning of objects in the container being
-// iterated. See the description of the Iterator struct for important information about how to
-// implement the cloning API.
-//
-// To build a CloningIterator you are supposed to invoke Iterator_make to intialize the
-// "standard" part of the iterator and then use it with `CloningIterator_make` to fill-in
-// the ClongingIterator APIs.
-//
+/// @brief CloningIterator
+/// A cloning iterator supports cloning of objects in the container being
+/// iterated. See the description of the Iterator struct for important information about how to
+/// implement the cloning API.
+///
+/// To build a CloningIterator you are supposed to invoke Iterator_make to intialize the
+/// "standard" part of the iterator and then use it with `CloningIterator_make` to fill-in
+/// the ClongingIterator APIs.
+///
 
 Iterator CloningIterator_make(
   Iterator iterator,
