337c7e7fe66dfcd99b45f98453f28ac90d2b06d0
Roberto Esposito <roberto.esposito@unito.it>
2022-11-23 10:16:40 +0100

    Improved iterator documentation

--- C/include/iterator.h
+++ C/include/iterator.h
@@ -5,20 +5,25 @@
 /// @file iterator.h
 /// @brief The Iterator type is the basis for the cross-container support for iteration functions.
 ///
-/// Every container should implement a functions that builds an Iterator for the container.
-/// That functions should simply call Iterator_make in order to fill the details necessary
-/// to support iteration over that container.
+/// # Iterator Interface
+/// Every container should implement a function that builds an Iterator for the container.
+/// That function should call Iterator_make in order to fill the details necessary
+/// to support iteration over that container. Additional make functions are available to fill
+/// the details for the different iterator traits: RandomAccessIterator_make, BidirectionalIterator_make,
+/// MutableIterator_make, CloningIterator_make.
 ///
-/// Once such a function exist the container gain access to all iteration functions declared
+/// Once such a function exist, the container gains access to all iteration functions declared
 /// in iterator_functions.h (e.g., for_each, map, find_first, ...).
 ///
 ///
-/// Iterator type declaration
+/// ## Iterator type declaration
 ///
 /// The Iterator type stores all the function pointers needed to work with the iterator APIs.
-/// The Iterator struct can be actually thought as a meta-iterator interface since will not
-/// hold the actual iterators, but it will provide means to created and work with them using
+/// The Iterator struct can be actually thought as a meta-iterator interface since it will not
+/// hold the actual iterators. Instead, it will provide means to create and work with iterators using
 /// an interface that will be shared by all iterator implementations.
+///
+/// ### Basic Iterators
 ///
 /// The information that need to create a new Iterator is:
 /// - container: the actual container on which one needs to iterate
@@ -38,6 +43,7 @@
 ///        point in the same container and 0 otherwise
 /// - free: a function that takes in input the iterator object and deallocates it.
 ///
+/// ### Random Access Iterators
 /// A random access iterator must also privide the following function pointers:
 /// - move_to: a function that moves the iterator to a new place.
 ///       input: an instance of the iterator and the new position;
@@ -47,26 +53,23 @@
 /// NOTE: move_to must be return a valid object every time it is called with a position
 ///       belonging to the range [0, size-1]
 ///
-/// a bidirectional iterator  must also privide the following function pointers:
+/// ### Bidirectional Iterators
+/// A bidirectional iterator  must also privide the following function pointers:
 ///
 /// - prev: a function that moves the iterator back of one position.
 ///        input: the iterator instance;
 /// - to_end: a function that moves the iterator at the end of the container;
 ///        input: the iterator instance
-
-/// mutable iterators  must also privide the following function pointers:
+/// 
+/// ### Mutable Iterators
+/// Mutable iterators  must also privide the following function pointers:
 ///
 /// - set: a function that assign to the current pointed element the given value
 ///      input: an instance of the iterator and the value to be assigned
 ///
-/// cloning iterators  must also privide the following function pointers:
+/// ### Cloning Iterators
+/// Cloning iterators  must also privide the following function pointers:
 ///
-/// NOTE: cloning is only useful for value types. When a container stores pointers, cloning can
-/// (and for performance reasons should) degrade to simply returning the pointers stored in the
-/// container. To understand why this is the case, consider that the client of the api will store
-/// the result of the cloning operation into a pointer. Since that storage is sufficient to actually
-/// hold the data (i.e., the pointer in the container) it is not necessary to alloc memory just to
-/// hold the pointer.
 ///  - alloc_obj: a function that allocates enough memory to store a new instance of the objects
 ///       contained in the underlying container
 ///       input: an instance of the iterator
@@ -78,6 +81,13 @@
 ///          received as input (in that case the function will have copied the current object into the
 ///          pointed memory)
 ///  - free_obj: frees the alloced memory (if necessary)
+///      
+/// NOTE: cloning is only useful for value types. When a container stores pointers, cloning can
+/// (and for performance reasons should) degrade to simply returning the pointers stored in the
+/// container. To understand why this is the case, consider that the client of the api will store
+/// the result of the cloning operation into a pointer. Since that storage is sufficient to actually
+/// hold the data (i.e., the pointer in the container) it is not necessary to alloc memory just to
+/// hold the pointer.
 
 typedef struct _Iterator Iterator;
 
@@ -112,16 +122,32 @@ struct _Iterator {
 
 // The following functions can be used to verify if an iterator satisfy a given piece of the
 // Iterator interface.
+
+/// @brief Returns 1 if the iterator is a bidirectional iterator and 0 otherwise.
 int is_bidirectional_iterator(Iterator it);
+
+/// @brief Returns 1 if the iterator is a random access iterator and 0 otherwise.
 int is_random_access_iterator(Iterator it);
+
+/// @brief Returns 1 if the iterator is a mutable iterator and 0 otherwise.
 int is_mutable_iterator(Iterator it);
+
+/// @brief Returns 1 if the iterator is a cloning iterator and 0 otherwise.
 int is_cloning_iterator(Iterator it);
 
 // The following functions can be used to generate an error and halt the program if an iterator
 // does not satisfy a given piece of the Iterator interface.
+
+/// @brief If the iterator is not a bidirectional iterator, it will generate an error and halt the program.
 void require_bidirectional_iterator(Iterator it);
+
+/// @brief If the iterator is not a random access iterator, it will generate an error and halt the program.
 void require_random_access_iterator(Iterator it);
+
+/// @brief If the iterator is not a mutable iterator, it will generate an error and halt the program.
 void require_mutable_iterator(Iterator it);
+
+/// @brief If the iterator is not a cloning iterator, it will generate an error and halt the program.
 void require_cloning_iterator(Iterator it);
 
 
--- C/include/iterator_functions.h
+++ C/include/iterator_functions.h
@@ -4,104 +4,141 @@
 #include "array.h"
 #include "keys.h"
 
+/// @file iterator_functions.h
+/// # Iterator Functions
+/// This file contains a number of functions that leverages the Iterator interface (see iterator.h)
+/// to provide facilities to iterate over containers.
+///
+/// Some function can be used on basic iterators, others needs specific traits to be implemented.
+///
+/// ## Functions requiring basic iterators
+///
+/// - first();
+/// - for_each();
+/// - filter();
+/// - for_each_with_index();
+/// - find_first();
+/// - map();
+/// - free_contents();
+///
+/// ## Functions requiring bidirectional iterators
+///
+/// - reverse();
+/// - find_last();
+/// - last();
+///
+/// ## Functions requiring random access iterators
+///
+/// - count();
+/// - binsearch();
+/// - binsearch_approx();
+///
+/// ## Functions requiring mutable iterators
+///
+/// - replace();
+///
+/// ## Functions requiring a combination of iterator traits
+///
+/// - reverse_contents();
+/// - sort();
+
 // --------------------------------------------------------------------------------
-// Functions supported by all iterators
+// Functions requiring basic iterators
 // --------------------------------------------------------------------------------
 
-// Returns the first element returned by the iterator
+/// @brief Returns the first element returned by the iterator
 void* first(Iterator it);
 
-// Calls callback on each element of the container iterated by the given Iterator
+/// Calls callback on each element of the container iterated by the given Iterator
 void for_each(Iterator, void (^callback)(void* elem));
 
-// Builds a new array from the elements accessible through
-// the given iterator. In the results will be placed only those
-// elements for which the given callback returns 1.
-// Note: The elements are not duplicated so the elements contained
-//   in the result should *not* be freed (unless the user needs that
-//   for some reason).
+/// Builds a new array from the elements accessible through
+/// the given iterator. In the results will be placed only those
+/// elements for which the given callback returns 1.
+/// Note: The elements are not duplicated so the elements contained
+///   in the result should *not* be freed (unless the user needs that
+///   for some reason).
 Array* filter(Iterator, int (^)(void*));
 
-// Calls callback on each element of the container iterated by the given Iterator
-// it passes to the callback the current element and the current index.
+/// Calls callback on each element of the container iterated by the given Iterator
+/// it passes to the callback the current element and the current index.
 void for_each_with_index(Iterator it, void(^callback)(void*, size_t));
 
-// Finds the first element matching the condition evaluated by the callback
-// `condition` must evaluate to 1 if the given elem is the one sought.
-// it must evaluate to 0 otherwise.
-// It returns NULL if no element satisfying the given condition is found.
+/// Finds the first element matching the condition evaluated by the callback
+/// `condition` must evaluate to 1 if the given elem is the one sought.
+/// it must evaluate to 0 otherwise.
+/// It returns NULL if no element satisfying the given condition is found.
 void* find_first(Iterator, int(^condition)(void* elem));
 
-// Use the given function pointer to map into a new object each object in the container iterated by
-// the given iterator. It returns the array containing the mapped objects. The memory of the
-// returned Array needs to be freed by the user.
+/// Use the given function pointer to map into a new object each object in the container iterated by
+/// the given iterator. It returns the array containing the mapped objects. The memory of the
+/// returned Array needs to be freed by the user.
 Array* map(Iterator, void* (^)(void*));
 
-// Frees the content of the given iterator using Mem_free
+/// Frees the content of the given iterator using Mem_free
 void free_contents(Iterator);
 
 // --------------------------------------------------------------------------------
-// Functions supported by bidirectional iterators
+// Functions requiring bidirectional iterators
 // --------------------------------------------------------------------------------
 
-// Reverses the direction of the given iterator. I.e., it swaps the next and prev 
-// functions.
+/// Reverses the direction of the given iterator. I.e., it swaps the next and prev 
+/// functions.
+/// @warning it requires the iterator to be bidirectional
 Iterator reverse(Iterator);
 
-// Finds the last element matching the condition evaluated by the callback
-// `condition` must evaluate to 1 if the given elem is the one sought.
-// it must evaluate to 0 otherwise.
-//
-// Requires a bidirectional iterator
-void* find_last(Iterator, int(^condition)(void* elem));
+/// Finds the last element matching the condition evaluated by the callback
+/// `condition` must evaluate to 1 if the given elem is the one sought.
+/// it must evaluate to 0 otherwise.
+///
+/// @warning it requires the iterator to be bidirectional
+void *find_last(Iterator, int (^condition)(void *elem));
 
-// Returns the last element returned by the iterator.
-//
-// Requires a bidirectional iterators
-// It supports non-bidirectional iterators with O(n) time complexity.
+/// Returns the last element returned by the iterator.
+///
+/// @warning if the iterator is bidirectional the operation is O(1), otherwise
+///    it supports non-bidirectional iterators with O(n) time complexity.
 void* last(Iterator it);
 
 // --------------------------------------------------------------------------------
-// Functions supported by random iterators
+// Functions requiring random access iterators
 // --------------------------------------------------------------------------------
 
-// Return the number of elements on which it iterates.
-//
-// Requires a random access iterator
-// It supports non-random iterators with O(n) time complexity.
+/// Return the number of elements on which it iterates.
+///
+/// @warning it requires a random access iterator for O(1) time complexity. It supports other iterators with O(n) time complexity.
 size_t count(Iterator it);
 
-// Finds an occurrence of the given elem. It returns the index of the found element or (size_t) -1.
-//
-// Requires a random access iterator over a sorted container.
-size_t binsearch(Iterator it, const void* elem, KIBlkComparator compare);
+/// Finds an occurrence of the given elem. It returns the index of the found element or (size_t) -1.
+///
+/// @warning it requires a random access iterator
+size_t binsearch(Iterator it, const void *elem, KIBlkComparator compare);
 
-// Finds an occurrence of the given elem. It returns the index of the nearest element found.
-//
-// Requires a random access iterator over a sorted container.
+/// Finds an occurrence of the given elem. It returns the index of the nearest element found.
+///
+/// @warning it requires a random access iterator **and** a sorted container
 size_t binsearch_approx(Iterator it, const void *elem, KIBlkComparator compare);
 
+/// --------------------------------------------------------------------------------
+/// Functions requiring mutable iterators
+/// --------------------------------------------------------------------------------
+
+/// Replaces the objects in the container with those returned by the given
+/// function.
+///
+/// @warning it requires a mutable iterator
+void replace(Iterator it, void * (^)(void *));
+
 // --------------------------------------------------------------------------------
-// Functions supported by mutable iterators
+// Functions requiring a combination of iterators
 // --------------------------------------------------------------------------------
 
-    // Replaces the objects in the container with those returned by the given
-    // function.
-    //
-    // Requires a mutable iterator.
-    void replace(Iterator it, void * (^)(void *));
-
-// --------------------------------------------------------------------------------
-// Functions supported by a combination of iterators
-// --------------------------------------------------------------------------------
-
-// Reverses the content of the given iterator.
-//
-// Requires a bidirectional mutable iterator.
+/// Reverses the content of the given iterator.
+///
+/// @warning it requires a bidirectional and mutable iterator.
 void reverse_contents(Iterator it);
 
-// Sorts the elements iterated by it according to the order induced by the compare function.
-// The function requires "it" to be a mutable and cloning iterator.
-// If "it" is also a random access iterator, the function switches to an implementation that
-// uses less memory and runs faster.
+/// Sorts the elements iterated by it according to the order induced by the compare function.
+/// @warning it requires a mutable and cloning iterator. If "it" is also a random access iterator, 
+///     the function switches to an implementation that uses less memory and runs faster.
 void sort(Iterator it, KIBlkComparator compare);
